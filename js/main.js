dc997f3c3980e3efe3c281bcc62e46e6ccff5c57
Kevin <GreenApple76@users.noreply.github.com>
2018-10-04 18:17:46 -0400

    retrieve puzzle words

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
+    var words = []; // store puzzle words retrieved from ajax request
+    
+    // retrieve list of puzzle words
+    $.ajax({
+        url: 'https://cors-anywhere.herokuapp.com/https://api.datamuse.com/words?sp=?????&max=10',
+        type: 'GET',
+        success: function(json) {
+            // populate words array with retrieved words
+            $.each(json, function() {
+                words.push(this.word);
+            })
+        },
+        complete: playGame 
+    });
 
+    function playGame() {
+        var randomWord = Math.floor(Math.random*10);
+        var puzzleWord = words[randomWord];
+
+    }
 });
